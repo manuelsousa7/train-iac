49c923e0d2d6a4be5b5f8760dc0c00f0fec61f69
Manuel Sousa <manuelsousamvs@gmail.com>
2015-10-29 14:24:42 +0000

    update

--- projeto_codigo.asm
+++ projeto_codigo.asm
@@ -3,9 +3,9 @@
 ; *													      *
 ; *										  			      *
 ; * Modulo:    projeto_codigo.asm						     	 *
-; *	Membros: - Gonçalo Marques ()                                     *
-; *			 - Manuel Sousa ()                                      *
-; *			 - Tiago Novais ()					      	      *
+; *	Membros: - Gonçalo Marques (84719)                                     *
+; *			 - Manuel Sousa (84740)                                      *
+; *			 - Tiago Novais (84888)					      	      *
 ; *									        				 *
 ; *													      *
 ; **********************************************************************
@@ -21,36 +21,75 @@
 ; *                           C Ó D I G O                             *             
 ; *********************************************************************
 
+; endereços dos portos do módulo dos comboios (apenas os usados)
+BOTOES_PRESSAO	     EQU  800CH	; endereço dos botões de pressão		
+SEMAFOROS 	     EQU  8012H	; endereço dos semáforos		
 
-PLACE		0000H               ; localiza código a partir do endereço 0000H (onde o PEPE começa a executar após reset)
+SEM_VERDE           EQU  2         ; cor dos semáforos (verde)
+SEM_VERMELHO        EQU  1         ; cor dos semáforos (vermelho)
+SEM_CINZENTO        EQU  0         ; cor dos semáforos (cinzento)
+
+; **********************************************************************
+; * Dados (variáveis)
+; **********************************************************************
+
+PLACE     1000H
+
+cores_semaforos:                   ; tabela para as cores dos semáforos (VERDE, CINZENTO ou VERMELHO).
+                                   ; Por omissão, todos os semáforos são mostrados inicialmente a VERDE
+     STRING    SEM_VERDE           ; cor do semáforo 0
+     STRING    SEM_VERDE           ; cor do semáforo 1
+     STRING    SEM_VERDE           ; cor do semáforo 2
+     STRING    SEM_VERDE           ; cor do semáforo 3
+     STRING    SEM_VERDE           ; cor do semáforo 4
+     STRING    SEM_VERDE           ; cor do semáforo 5
+     STRING    SEM_VERDE           ; cor do semáforo 6
+     STRING    SEM_VERDE           ; cor do semáforo 7
+     STRING    SEM_VERDE           ; cor do semáforo 8
+     STRING    SEM_VERDE           ; cor do semáforo 9
+
+
+
+; **********************************************************************
+; * Código
+; **********************************************************************
+
+PLACE		0000H
 inicio:
 
-; acesso à memória, em modo word (16 bits) e em modo byte (8 bits)
-     MOV	R1, VALOR_16_BITS	     ; inicializa valor a escrever
-     MOV	R9, END_ACESSO_16	     ; endereço (necessariamente par) a ser acedido em modo word (16 bits)
-     MOV	[R9], R1	               ; acesso (MOV) à RAM em 16 bits
+     MOV	R9, BOTOES_PRESSAO       ; endereço do porto dos botões de pressão
 
-     MOV	R9, END_ACESSO_8_PAR     ; endereço (par) a ser acedido em modo byte (8 bits)
-     MOVB	[R9], R1	               ; acesso (MOVB) à RAM em 8 bits (só o byte de menor peso do R1)
+le_botoes:
+     MOVB	R1, [R9]                 ; lê o estado dos botões (modo byte, pois o periférico é de 8 bits)
+     BIT  R1, 5                    ; testa o botão de pressão 5
+     JZ   le_botoes                ; se o bit está a zero, o botão não está carregado e continua a ler os botões
 
-     MOV	R9, END_ACESSO_8_IMPAR   ; endereço (impar) a ser acedido em modo byte (8 bits)
-     MOVB	[R9], R1	               ; acesso (MOVB) à RAM em 8 bits (só o byte de menor peso do R1)
+botao_carregado:
+     MOV  R10, cores_semaforos     ; endereço da tabela das cores dos vários semáforos
+     MOV  R3, 5                    ; número do semáforo com que se quer trabalhar (5)
+     ADD  R10, R3                  ; obtém endereço do byte de cor do semáforo (soma 5 à base da tabela)
+     MOVB	R2, [R10]                ; lê a cor do semáforo (modo byte, pois a tabela de cores dos semáforos foi definida com STRING)
+     CMP  R2, SEM_VERDE
+     JZ   poe_vermelho             ; se o semáforo está a verde, põe a vermelho, caso contrário põe a verde
+     
+poe_verde:
+     MOV  R4, SEM_VERDE            ; semáforo via ficar verde
+     JMP  atualiza_cor
+     
+poe_vermelho:
+     MOV  R4, SEM_VERMELHO         ; semáforo via ficar vermelho
+     
+atualiza_cor:
+     MOVB [R10], R4                ; atualiza cor do semáforo na tabela (modo byte)
 
-; acesso ao periférico (só em modo byte, pois o periférico é de 8 bits)
-     MOV	R9, LCD_CIMA             ; endereço do porto 00H do módulo dos comboios (linha de cima do LCD)
-     MOV	R1, CARACTER_A           ; caracter 'A' em ASCII
-     MOVB	[R9], R1                 ; escreve carácter no LCD
+atualiza_semaforo:
+     SHL  R3, 2                    ; formato do porto dos semáforos (número do semáforo tem de estar nos bits 7 a 2, cor nos bits 1 e 0)
+     ADD  R3, R4                   ; junta cor do semáforo (que fica nos bits 1 e 0)
+     MOV  R11, SEMAFOROS           ; endereço do porto dos semáforos no módulo dos comboios
+     MOVB [R11], R3                ; atualiza cor no semaforo propriamente dito (modo byte)
 
-     MOV	R9, LCD_BAIXO            ; endereço do porto 01H do módulo dos comboios (linha de baixo do LCD)
-     MOV	R1, CARACTER_D           ; caracter 'D' em ASCII
-     MOVB	[R9], R1                 ; escreve carácter no LCD
-
-     MOV	R9, AGULHAS              ; endereço do porto 0BH do módulo dos comboios (agulhas)
-     MOV	R1, AGULHA_2_ESQ         ; agulha 2 para a esquerda
-     MOVB	[R9], R1                 ; muda estado da agulha
-
-     MOV	R9, SEMAFOROS            ; endereço do porto 09H do módulo dos comboios (semáforos)
-     MOV	R1, SEM_5_VERMELHO       ; semáforo 5 a vermelho
-     MOVB	[R9], R1                 ; muda estado do semáforo
-
-fim: JMP  fim                      ; "termina" o programa
+bascula:
+	 MOVB R1,[R9]
+	 BIT R1, 5
+	 JNZ bascula
+     JMP  le_botoes                ; vai novamente ler os botões
