5aad206bb7029f86332f73dc56d93516da7fb546
goncasmage1 <goncalo.cool@gmail.com>
2015-11-03 17:31:52 +0000

    Update
    Rotina das agulhas e ciclo dos semáforos (incompleto)

--- projeto_codigo.asm
+++ projeto_codigo.asm
@@ -11,17 +11,20 @@
 ; **********************************************************************
 
 
-
-
-
 ; **********************************************************************
 ; * Constantes
 ; **********************************************************************
 
 ; endereços dos portos do módulo dos comboios (apenas os usados)
+
 LCD_CIMA			EQU  8000H	; endereço do LCD de cima
+LCD_BAIXO			EQU 8002H ; endereço do LCD de baixo
+SLIDERS			EQU 8004H ; endereço dos sliders
+TECLADO_1			EQU 8006H ; endereço dos botões 0 a 7
+TECLADO_2			EQU 8008H ; endereço dos botões de 8 a F
 BOTOES_PRESSAO	     EQU  800CH	; endereço dos botões de pressão		
-SEMAFOROS 	     EQU  8012H	; endereço dos semáforos		
+SEMAFOROS 	     EQU  8012H	; endereço dos semáforos
+AGULHAS				EQU 8016H; endereço das agulhas		
 SELECAO	          EQU  8018H	; endereço do porto de seleção de comboio e comando		
 COMANDO		     EQU  801AH	; endereço do porto onde dar comandos para os comboios		
 SENSOR_EVENTOS	     EQU  801CH	; endereço do porto dos sensores (número de eventos)		
@@ -54,8 +57,19 @@ cores_semaforos:                   ; tabela para as cores dos semáforos (VERDE,
      STRING    SEM_VERDE           ; cor do semáforo 8
      STRING    SEM_VERDE           ; cor do semáforo 9
 
-estado_botao_5:
-     WORD      0                   ; estado atual do processo botao_5
+estado_botoes:
+     WORD      0                   ; estado atual do botao 0
+     WORD      0                   ; estado atual do botao 1
+	 WORD      0                   ; estado atual do botao 2
+	 WORD      0                   ; estado atual do botao 3
+	 WORD      0                   ; estado atual do botao 4
+	 WORD      0                   ; estado atual do botao 5
+	 WORD      0                   ; estado atual do botao 6
+	 WORD      0                   ; estado atual do botao 7
+mascara_1:
+	WORD		1
+mascara_2:
+	WORD		3
 
 ; **********************************************************************
 ; * Código
@@ -64,20 +78,29 @@ estado_botao_5:
 PLACE		0000H
 inicio:
 
-     MOV	SP, SP_inicial           ; inicializa SP para a palavra a seguir à última da pilha
+    MOV	SP, SP_inicial           ; inicializa SP para a palavra a seguir à última da pilha
 
-     MOV  R0, 0                    ; número do comboio 0
-     MOV	R9, SELECAO
-     SHL  R0, 4                    ; nº do comboio: bits 7 a 4
+    MOV  R0, 0                    ; número do comboio 0
+    MOV	R9, SELECAO
+    SHL  R0, 4                    ; nº do comboio: bits 7 a 4
 	MOVB	[R9], R0				; o próximo comando vai alterar a velocidade do comboio indicado por R0
 	MOV	R9, COMANDO
-     MOV  R1, 3                    ; sentido para a frente, velocidade máxima (3)
+    MOV  R1, 3                    ; sentido para a frente, velocidade máxima (3)
 	MOVB	[R9], R1                 ; põe comboio a andar
+	
+	
+	
+	
+	
+	
+	MOV R6, 0					; vai guardar o indice do semáforo
+	;importante deixar assim
 
 ciclo:
-     CALL	botao_5                  ; testa o botão de pressão 5 e troca a cor do semáforo 5 quando for premido
-     CALL sensores                 ; testa sensores e escreve no LCD quando o comboio passar por um deles
-     JMP	ciclo
+    CALL botoes                  ; testa o botão de pressão 5 e troca a cor do semáforo 5 quando for premido
+    CALL sensores                 ; testa sensores e escreve no LCD quando o comboio passar por um deles
+	CALL agulhas
+    JMP ciclo
      
      
 ; **************************************************************************************************************
@@ -85,53 +108,72 @@ ciclo:
 ; ARGUMENTOS: nenhum
 ; RETORNA:    nada
 ; **************************************************************************************************************
-botao_5:
-     PUSH R1
-     PUSH R9
-     PUSH R10
-     PUSH R11
-
-     MOV  R10, estado_botao_5
-     MOV  R11, [R10]               ; obtém estado atual do processo botao_5
+botoes:
+    PUSH R1
+	PUSH R3
+	PUSH R4
+    PUSH R9
+    PUSH R10
+    PUSH R11
+	
+    MOV R10, estado_botoes
+	ADD R10, R6
+    MOVB R11, [R10]               ; obtém estado atual do botao x
+	MOV R3, mascara_1			
+	MOVB R4, [R3]				; le o valor da mascara_1
+	CMP R4, 80H				; vai ver se o bit a 1 da máscara já chegou ao índice 7
+	JZ botoes_0			; se nao chegou vai saltar a próxima instrução
+	MOVB R4, 80H				; se sim, vai por a mascara a 1H
      
-botao_5_0:					; estado 0 - À espera que o botão seja premido
+botoes_0:					; estado 0 - À espera que o botão seja premido
 	CMP	R11, 0				; estamos no estado 0?
-	JNZ	botao_5_1
+	JNZ	botoes_1
 
 espera_botao_premido:
-     MOV	R9, BOTOES_PRESSAO       ; endereço do porto dos botões de pressão
-     MOVB	R1, [R9]                 ; lê o estado dos botões (modo byte, pois o periférico é de 8 bits)
-     BIT  R1, 5                    ; testa o botão de pressão 5
-     JZ   botao_5_fim              ; se o bit está a zero, o botão não está carregado. Tem de esperar que seja premido
+    MOV	R9, TECLADO_1       ; endereço do porto do teclado_1
+    MOVB	R1, [R9]                 ; lê o estado dos botões (modo byte, pois o periférico é de 8 bits)
+    AND  R1, R4                    ; testa o botão de pressão x (indice depende da mascara_1)
+    JZ   botao_5_fim              ; se o bit está a zero, o botão não está carregado. Tem de esperar que seja premido
 
 botao_premido:                     ; botão foi premido! Pode trocar a cor e passar ao estado 1
-     MOV  R1, 5                    ; número do semáforo cujan cor é para trocar
+     MOV  R1, R4                    ; número do semáforo cujan cor é para trocar
      CALL troca_cor_semaforo       ; troca cor do semáforo
      
 	MOV	R11, 1
+	ADD R10, R6				;adiciona o indice do semáforo ao teclado_1
 	MOV	[R10], R11			; passa ao estado 1 (atualiza na variável)
-	JMP	botao_5_fim              ; mas só na próxima iteração. Agora sai
+	JMP	botoes_fim              ; mas só na próxima iteração. Agora sai
 
-botao_5_1:					; estado 1 - À espera que o botão seja libertado
+botoes_1:					; estado 1 - À espera que o botão seja libertado
 	CMP	R11, 1
-	JNZ	botao_5_fim              ; se estado desconhecido, sai e ignora
+	JNZ	botoes_fim              ; se estado desconhecido, sai e ignora
 
 espera_botao_livre:
-     MOV	R9, BOTOES_PRESSAO       ; endereço do porto dos botões de pressão
-     MOVB	R1, [R9]                 ; lê o estado dos botões (modo byte, pois o periférico é de 8 bits)
-     BIT  R1, 5                    ; testa o botão de pressão 5
-     JNZ  botao_5_fim              ; se o bit não está a zero, o botão está carregado. Tem de esperar que esteja não premido
+    MOV	R9, TECLADO_1       ; endereço do porto dos botões de pressão
+    MOVB R1, [R9]                 ; lê o estado dos botões (modo byte, pois o periférico é de 8 bits)
+    AND  R1, R4                    ; testa o botão de pressão x (indice depende da mascara)
+    JNZ  botoes_sub              ; se o bit não está a zero, o botão está carregado. Tem de esperar que não esteja premido
      
 	MOV	R11, 0
-	MOV	[R10], R11			; passa ao estado 0 (atualiza na variável)
-	JMP	botao_5_fim
+	MOV	[R10], R11				; passa ao estado 0 (atualiza na variável)
+	JMP	botoes_sub
      
-botao_5_fim:    
-     POP R11
-     POP R10
-     POP R9
-     POP R1
-     RET
+botoes_max:						;COMO EXECUTAR ESTES COMANDOS SÓ SE R4 == 80H??
+	MOV R4, 01H					;COMO EXECUTAR ESTES COMANDOS SÓ SE R4 == 80H??
+	JMP botoes_fim				;COMO EXECUTAR ESTES COMANDOS SÓ SE R4 == 80H??
+	 
+botoes_sub:
+	ROL R4,1
+botoes_fim:
+	MOV [R3], R4
+	ADD R6, 1
+    POP R11
+    POP R10
+    POP R9
+	POP R4
+	POP R3
+    POP R1
+    RET
 
 
 ; **************************************************************************************************************
@@ -140,9 +182,9 @@ botao_5_fim:
 ; RETORNA:    nada
 ; **************************************************************************************************************
 sensores:
-     PUSH R1
-     PUSH R2
-     PUSH R9
+    PUSH R1
+    PUSH R2
+    PUSH R9
 
 espera_eventos:
 	MOV	R9, SENSOR_EVENTOS
@@ -156,16 +198,16 @@ houve_evento:
 	MOVB	R2, [R9]		          ; lê 2º byte (número do sensor)
 
 escreve_numero_sensor:
-     MOV  R1, 30H                  ; '0'
-     ADD  R2, R1                   ; converte número do sensor (0 a 9) para caracter numérico ('0' a '9')
+    MOV  R1, 30H                  ; '0'
+    ADD  R2, R1                   ; converte número do sensor (0 a 9) para caracter numérico ('0' a '9')
 	MOV	R9, LCD_CIMA		     ; porto da linha de cima do LCD
-     MOVB [R9], R2                 ; escreve número do sensor no LCD
+    MOVB [R9], R2                 ; escreve número do sensor no LCD
 
 sensores_fim:
-     POP R9
-     POP R2
-     POP R1
-     RET
+    POP R9
+    POP R2
+    POP R1
+    RET
 
      
 ; **************************************************************************************************************
@@ -174,24 +216,24 @@ sensores_fim:
 ; RETORNA:    nada
 ; **************************************************************************************************************
 troca_cor_semaforo:
-     PUSH R2
-     CALL obtem_cor_semaforo       ; obtém cor do semáforo (em R2)
+    PUSH R2
+    CALL obtem_cor_semaforo       ; obtém cor do semáforo (em R2)
 
-     CMP  R2, SEM_VERDE
-     JZ   poe_vermelho             ; se o semáforo está a verde, põe a vermelho, caso contrário põe a verde
+    CMP  R2, SEM_VERDE
+    JZ   poe_vermelho             ; se o semáforo está a verde, põe a vermelho, caso contrário põe a verde
      
 poe_verde:
-     MOV  R2, SEM_VERDE            ; semáforo via ficar verde
-     JMP  atualiza_cor
+    MOV  R2, SEM_VERDE            ; semáforo via ficar verde
+    JMP  atualiza_cor
      
 poe_vermelho:
-     MOV  R2, SEM_VERMELHO         ; semáforo via ficar vermelho
+    MOV  R2, SEM_VERMELHO         ; semáforo via ficar vermelho
      
 atualiza_cor:
-     CALL atualiza_cor_semaforo    ; atualiza cor do semáforo na tabela e na interface.
+    CALL atualiza_cor_semaforo    ; atualiza cor do semáforo na tabela e na interface.
                                    ; R1 ainda tem o número do semáforo e R2 tem a nova cor
-     POP R2
-     RET
+    POP R2
+    RET
    
    
 ; **************************************************************************************************************
@@ -200,11 +242,11 @@ atualiza_cor:
 ; RETORNA:    R2 - cor do semáforo (SEM_VERDE, SEM_VERMELHO ou SEM_CINZENTO)
 ; **************************************************************************************************************
 obtem_cor_semaforo:
-     PUSH R10                      ; guarda valor do registo na pilha
-     MOV  R10, cores_semaforos     ; endereço da tabela das cores dos vários semáforos
-     ADD  R10, R1                  ; obtém endereço do byte de cor do semáforo (soma o número do semáforo à base da tabela)
-     MOVB	R2, [R10]                ; lê a cor do semáforo
-     POP R10                       ; repõe o valor anterior do registo a partir da cópia guardada na pilha
+    PUSH R10                      
+    MOV  R10, cores_semaforos     ; endereço da tabela das cores dos vários semáforos
+    ADD  R10, R6                  ; obtém endereço do byte de cor do semáforo (soma o número do semáforo à base da tabela)
+    MOVB R2, [R10]                ; lê a cor do semáforo
+    POP R10                       
 	RET
 	
      
@@ -216,29 +258,60 @@ obtem_cor_semaforo:
 ; RETORNA:    nada
 ; **************************************************************************************************************
 atualiza_cor_semaforo:
-     PUSH R1                       ; guarda valores dos registos na pilha
-     PUSH R10
-     PUSH R11
-     MOV  R10, cores_semaforos     ; endereço da tabela das cores dos vários semáforos
-     ADD  R10, R1                  ; obtém endereço do byte de cor do semáforo (soma o número do semáforo à base da tabela)
-     MOVB	[R10], R2                ; atualiza a cor do semáforo na tabela de cores dos semáforos
+    PUSH R5                       ; guarda valores dos registos na pilha
+    PUSH R10
+    PUSH R11
+    MOV  R10, cores_semaforos     ; endereço da tabela das cores dos vários semáforos
+    ADD  R10, R5                  ; obtém endereço do byte de cor do semáforo (soma o número do semáforo à base da tabela)
+    MOVB	[R10], R2             ; atualiza a cor do semáforo na tabela de cores dos semáforos
 
-     SHL  R1, 2                    ; formato do porto dos semáforos (número do semáforo tem de estar nos bits 7 a 2, cor nos bits 1 e 0)
-     ADD  R1, R2                   ; junta cor do semáforo (que fica nos bits 1 e 0)
-     MOV  R11, SEMAFOROS           ; endereço do porto dos semáforos no módulo dos comboios
-     MOVB [R11], R1                ; atualiza cor no semaforo propriamente dito
-     POP R11                       ; repõe os valores anteriores dos registos a partir das cópias guardadas na pilha
-     POP R10
-     POP R1
+    SHL  R5, 2                    ; formato do porto dos semáforos (número do semáforo tem de estar nos bits 7 a 2, cor nos bits 1 e 0)
+    ADD  R5, R2                   ; junta cor do semáforo (que fica nos bits 1 e 0)
+    MOV  R11, SEMAFOROS           ; endereço do porto dos semáforos no módulo dos comboios
+    MOVB [R11], R5                ; atualiza cor no semaforo propriamente dito
+    POP R11                       ; repõe os valores anteriores dos registos a partir das cópias guardadas na pilha
+    POP R10
+    POP R5
 	RET
 
-bascula:
-	 MOVB R1,[R9]	
-	 BIT R1, 1
-	 JNZ bascula
-     JMP  le_botoes                ; vai novamente ler os botões
 				
+; **************************************************************************************************************
+; agulhas - atualiza o estado das agulhas
+; **************************************************************************************************************
 
+agulhas:
+	PUSH R2
+	PUSH R3
+	PUSH R4
+	PUSH R7
+	PUSH R8
+	PUSH R9
+	
+	MOV R9, BOTOES_PRESSAO
+	MOV R8, [R9]
+	BIT R8,0
+	JZ agulhas_fim
+	
+	MOV R7, MASCARA_2
+	MOV R3, AGULHAS
+	MOVB R2, [R3]
+	MOVB R4, [R3]
+	SHL R2, 6
+	SHR R2, 6
+	XOR R2, R7
+	SHR R4, 2
+	SHL R4, 2
+	ADD R4, R2
+	MOV [R3], R4
+	
+agulhas_fim:
+	POP R9
+	POP R8
+	POP R7
+	POP R4
+	POP R3
+	POP R2
+	RET
 
 
 
